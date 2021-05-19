44148025b0bb4b78eb017716ea7263ab48b9713b
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-20 05:28:31 +0545

    Creates CRUD for roles

--- controllers/roles.controller.js
+++ controllers/roles.controller.js
@@ -12,5 +12,47 @@ module.exports = {
             })
             return
         }
+    },
+    async createRole(req, res, next) {
+        try {
+            result = await pg.getPool().query(
+                "INSERT INTO roles(name, description) VALUES ($1, $2) RETURNING *",
+                [req.body.name, req.body.description])
+            res.send(result.rows)
+        } catch (err) {
+            console.log(res)
+            res.status(500).json({
+                message: err.message
+            })
+            return
+        }
+    },
+    async updateRole(req, res, next) {
+        try {
+            result = await pg.getPool().query(
+                "UPDATE roles SET name = $1, description = $2 WHERE ID = $3 RETURNING *",
+                [req.body.name, req.body.description, req.params.id])
+            res.send(result.rows)
+        } catch (err) {
+            console.log(res)
+            res.status(500).json({
+                message: err.message
+            })
+            return
+        }
+    },
+    async deleteRole(req, res, next) {
+        try {
+            result = await pg.getPool().query(
+                "DELETE FROM roles WHERE ID = $1 RETURNING *",
+                [req.params.id])
+            res.send(result.rows)
+        } catch (err) {
+            console.log(res)
+            res.status(500).json({
+                message: err.message
+            })
+            return
+        }
     }
 }
--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/roles', rolesController.getRoles)
+app.post('/roles', rolesController.createRole)
+app.put('/roles/:id', rolesController.updateRole)
+app.delete('/roles/:id', rolesController.deleteRole)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
