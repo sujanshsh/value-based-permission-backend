64196b7c2e647da17775951165d300ced7a95276
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-20 09:02:24 +0545

    Fixes missing const keyword

--- controllers/roles.controller.js
+++ controllers/roles.controller.js
@@ -3,7 +3,7 @@ import getPool from '../services/pgService.js'
 export default class RolesController {
     static async getRoles(req, res, next) {
         try {
-            result = await getPool().query("SELECT * FROM roles")
+            const result = await getPool().query("SELECT * FROM roles")
             res.send(result.rows)
         } catch (err) {
             res.status(500).json({
@@ -14,7 +14,7 @@ export default class RolesController {
 
     static async createRole(req, res, next) {
         try {
-            result = await getPool().query(
+            const result = await getPool().query(
                 "INSERT INTO roles(name, description) VALUES ($1, $2) RETURNING *",
                 [req.body.name, req.body.description])
             res.send(result.rows)
@@ -27,7 +27,7 @@ export default class RolesController {
 
     static async updateRole(req, res, next) {
         try {
-            result = await getPool().query(
+            const result = await getPool().query(
                 "UPDATE roles SET name = $1, description = $2 WHERE ID = $3 RETURNING *",
                 [req.body.name, req.body.description, req.params.id])
             res.send(result.rows)
@@ -40,7 +40,7 @@ export default class RolesController {
 
     static async deleteRole(req, res, next) {
         try {
-            result = await getPool().query(
+            const result = await getPool().query(
                 "DELETE FROM roles WHERE ID = $1 RETURNING *",
                 [req.params.id])
             res.send(result.rows)
