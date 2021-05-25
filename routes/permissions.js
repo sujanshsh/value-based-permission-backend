73030b53c7ed7e4173623d047831dc0b0a600981
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-25 08:16:22 +0545

    Adds route for permissions like search values

--- routes/permissions.js
+++ routes/permissions.js
@@ -5,6 +5,7 @@ import PermissionsController from '../controllers/permissions.controller.js'
 const permissionsRouter = express.Router()
 
 permissionsRouter.get('/permissions', PermissionsController.getPermissions)
+permissionsRouter.get('/permissions/like', PermissionsController.getPermissionsLike)
 permissionsRouter.post('/permissions', PermissionsController.createPermission)
 permissionsRouter.put('/permissions/:id', PermissionsController.updatePermission)
 permissionsRouter.delete('/permissions/:id', PermissionsController.deletePermission)
