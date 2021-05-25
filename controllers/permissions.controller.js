7f2bfcdc964839ec1bc28f45116452cbf3b6c47f
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-25 08:09:14 +0545

    Adds search functionality to permisions

--- controllers/permissions.controller.js
+++ controllers/permissions.controller.js
@@ -3,7 +3,62 @@ import getPool from '../services/pgService.js'
 export default class PermissionsController {
     static async getPermissions(req, res, next) {
         try {
-            const result = await getPool().query("SELECT * FROM permissions")
+            let whereClause = ''
+            let whereConditions = []
+            let index = 0
+            let bindVars = []
+            if (req.query.name) {
+                index++
+                whereConditions.push(`name = $${index}`)
+                bindVars.push(req.query.name)
+            }
+            if (req.query.description) {
+                index++
+                whereConditions.push(`description = $${index}`)
+                bindVars.push(req.query.description)
+            }
+            if (req.query.values) {
+                index++
+                whereConditions.push(`values = $${index}`)
+                bindVars.push(req.query.values)
+            }
+            if (whereConditions.length > 0) {
+                whereClause = 'WHERE ' + whereConditions.join(' AND ')
+            }
+            const result = await getPool().query(`SELECT * FROM permissions ${whereClause}`, bindVars)
+            res.send(result.rows)
+        } catch (err) {
+            res.status(500).json({
+                message: err.message
+            })
+        }
+    }
+
+    static async getPermissionsLike(req, res, next) {
+        try {
+            let whereClause = ''
+            let whereConditions = []
+            let index = 0
+            let bindVars = []
+            if (req.query.name) {
+                index++
+                whereConditions.push(`LOWER(name) LIKE $${index}`)
+                bindVars.push(req.query.name.toLowerCase() + '%')
+            }
+            if (req.query.description) {
+                index++
+                whereConditions.push(`LOWER(description) LIKE $${index}`)
+                bindVars.push(req.query.description.toLowerCase() + '%')
+            }
+            if (req.query.values) {
+                index++
+                whereConditions.push(`LOWER(values) LIKE $${index}`)
+                bindVars.push(req.query.values.toLowerCase() + '%')
+            }
+            if (whereConditions.length > 0) {
+                whereClause = 'WHERE ' + whereConditions.join(' OR ')
+            }
+            const result = await getPool().query(`SELECT * FROM permissions ${whereClause}`, bindVars)
             res.send(result.rows)
         } catch (err) {
             res.status(500).json({
